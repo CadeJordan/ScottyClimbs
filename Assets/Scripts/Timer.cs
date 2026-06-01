5a3536c008022e28529448ee5eac24960ecccbb0
Quin Gill <qhgill2@gmail.com>
2026-06-01 15:31:34 -0700

    time save

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,6 +2,17 @@ using UnityEngine;
 using Time = UnityEngine.Time;
 using TMPro;
 using System;
+using System.IO;
+
+[System.Serializable]
+public class BestTimeData
+{
+    public int minutes;
+    public int seconds;
+    public int milliseconds;
+    public float totalTime;
+}
+
 public class Timer : MonoBehaviour
 {
     public float time = 0;
@@ -11,11 +22,15 @@ public class Timer : MonoBehaviour
     public TextMeshProUGUI timer;
     public Collider targetTrigger;
     private bool timerRunning = true;
+    
+    private string bestTimeFilePath;
+    private BestTimeData bestTimeData;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        bestTimeFilePath = Application.persistentDataPath + "/bestTime.json";
+        LoadBestTime();
     }
 
     // Update is called once per frame
@@ -37,6 +52,40 @@ public class Timer : MonoBehaviour
         if (collision == targetTrigger)
         {
             timerRunning = false;
+            SaveBestTime();
+        }
+    }
+
+    void LoadBestTime()
+    {
+        if (File.Exists(bestTimeFilePath))
+        {
+            string json = File.ReadAllText(bestTimeFilePath);
+            bestTimeData = JsonUtility.FromJson<BestTimeData>(json);
+            Debug.Log($"Best time loaded: {bestTimeData.minutes:00}:{bestTimeData.seconds:00}:{bestTimeData.milliseconds:000}");
+        }
+        else
+        {
+            bestTimeData = new BestTimeData { minutes = 0, seconds = 0, milliseconds = 0, totalTime = float.MaxValue };
+            Debug.Log("No best time file found. Starting fresh.");
+        }
+    }
+
+    void SaveBestTime()
+    {
+        if (bestTimeData == null || time < bestTimeData.totalTime)
+        {
+            bestTimeData = new BestTimeData
+            {
+                minutes = minutes,
+                seconds = seconds,
+                milliseconds = milliseconds,
+                totalTime = time
+            };
+
+            string json = JsonUtility.ToJson(bestTimeData, true);
+            File.WriteAllText(bestTimeFilePath, json);
+            Debug.Log($"New best time saved: {bestTimeData.minutes:00}:{bestTimeData.seconds:00}:{bestTimeData.milliseconds:000}");
         }
     }
 }
